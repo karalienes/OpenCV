765370443c5d9817bb91882a2b4777014c711aef
Enes KARALİ <38677441+karalienes@users.noreply.github.com>
2020-03-04 05:20:20 +0300

    Add files via upload

---
+++ opencv1/cmake-build-debug/CMakeFiles/opencv1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv1.dir/main.cpp.o"
+  "opencv1.pdb"
+  "opencv1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
